338064a47988c66ebd523e3c822dfa90e583b7b6
Clemens von Schwerin <clemens.vonschwerin@knime.com>
2018-01-12 13:59:37 +0100

    AP-8534: Cleanup

--- org.knime.core.streaming/src/org/knime/core/streaming/inoutput/SharedContainerOutputCache.java
+++ org.knime.core.streaming/src/org/knime/core/streaming/inoutput/SharedContainerOutputCache.java
@@ -22,7 +22,7 @@
  *  Hence, KNIME and ECLIPSE are both independent programs and are not
  *  derived from each other. Should, however, the interpretation of the
  *  GNU GPL Version 3 ("License") under any applicable laws result in
- *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
  *  you the additional permission to use and propagate KNIME together with
  *  ECLIPSE with only the license terms in place for ECLIPSE applying to
  *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
@@ -70,7 +70,7 @@ import org.knime.core.node.workflow.SingleNodeContainer;
  * a {@link BufferedDataTable}. The encapsulated object is shared between the producing and the consuming node and
  * is update by the producer and processed by the consumer multiple times. It is taken care of synchronization.
  *
- * @author Clemens von Schwerin, University of Ulm
+ * @author Clemens von Schwerin, KNIME, Konstanz, Germany
  */
 public class SharedContainerOutputCache extends AbstractOutputCache<SharedContainerPortObjectSpec> {
 
